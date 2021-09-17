5e80f5f021f3686ec4df2e307058d3ae9a395be0
Viczdera <vichizsoft@yahoo.com>
2021-09-17 01:14:59 +0100

    domain for frontend

--- socket/index.js
+++ socket/index.js
@@ -1,7 +1,7 @@
 const port= process.env.PORT||8000
 const io = require("socket.io")(port, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://my-messenger-zeta.vercel.app/",
   },
 });
 
