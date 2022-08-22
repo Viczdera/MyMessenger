ed91232d3fc87764e65abfa70509cf64664ee20e
Viczdera <vichizsoft@yahoo.com>
2022-08-22 11:23:52 +0100

    sign up verification mod

--- pages/api/apiCalls.js
+++ pages/api/apiCalls.js
@@ -1,31 +1,31 @@
-import axios from "axios"
-import { addToLocalStorage } from "../../utils/browserStorage"
+import axios from "axios";
+import { useEffect } from "react";
+import { addToLocalStorage } from "../../utils/browserStorage";
+
+export const LoginCall = async (userCred, dispatch) => {
 
 
-export const LoginCall= async(userCred,dispatch)=>{
-    dispatch({type:"LOGIN_START"})
-    //MAKE REQUEST
+  dispatch({ type: "LOGIN_START" });
+  //MAKE REQUEST
 
-    try{
-        const res= await axios.post("api/login", userCred)
-        dispatch ({type:"LOGIN_SUCCESS",payload:res.data})
-       // localStorage.setItem("user",JSON.stringify(res.data))
-        addToLocalStorage("user",res.data)
+  try {
+    const res = await axios.post("api/login", userCred);
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    // localStorage.setItem("user",JSON.stringify(res.data))
+    addToLocalStorage("user", res.data);
 
+   
+  } catch (err) {
+    dispatch({ type: "LOGIN_FAILURE", payload: "error" });
+    prompt("login error chap");
+  }
+};
 
-    }catch(err){
-        dispatch ({type:"LOGIN_FAILURE",payload:"error"})
-        prompt("login error chap")
-        
-    }
-} 
-
-export const Signupcall= async(user)=>{
-    try{
-        const res = await axios.post("api/register", user)
-        console.log(res.data)
-    }catch(err){
-        console.log(err)
-    }
-
-}
+export const Signupcall = async (user) => {
+  try {
+    const res = await axios.post("api/register", user);
+    console.log(res.data);
+  } catch (err) {
+    console.log(err);
+  }
+};
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,26 +1,36 @@
+/* eslint-disable import/no-anonymous-default-export */
 import connectDB from "../../utils/connectDB";
-import User from "../../models/User"
-import bcrypt from "bcrypt"
+import User from "../../models/User";
+import bcrypt from "bcrypt";
 
 connectDB();
 
 export default async (req, res) => {
-  const {method}=req
+  const { method } = req;
   switch (method) {
     case "POST":
       try {
         //encrypt
-        const msalt= await bcrypt.genSalt(10)
-        const passHashed= await bcrypt.hash(req.body.password, msalt)
-        const regUser = await User.create({
-          name:req.body.name,
-          email: req.body.email,
-          password:passHashed,
+        const msalt = await bcrypt.genSalt(10);
+        const passHashed = await bcrypt.hash(req.body.password, msalt);
+        const userExist = await User.findOne({
+          $or: [{ email: email }, { phone: phone }],
         });
-        const user= await regUser.save();
+        const regUser = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: passHashed,
+        });
+        if (userExist) {
+          return res
+            .status(400)
+            .json({ error: "Email or Phone number already exists" });
+        }
+
+        const user = await regUser.save();
         res.status(200).json({
           success: true,
-          data: user
+          data: user,
         });
       } catch (err) {
         res.status(500).json({ success: false });
