2d53b1acf3a98bd1dd9098a96474a235f47ac74a
Viczdera <vichizsoft@yahoo.com>
2022-08-22 12:01:52 +0100

    sign up verification mod final

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,39 +1,37 @@
 /* eslint-disable import/no-anonymous-default-export */
 import connectDB from "../../utils/connectDB";
-import User from "../../models/User";
-import bcrypt from "bcrypt";
+import User from "../../models/User"
+import bcrypt from "bcrypt"
 
 connectDB();
 
 export default async (req, res) => {
-  const { method } = req;
+  const {method}=req
   switch (method) {
     case "POST":
       try {
+        const { name, email, password} = req.body
         //encrypt
-        const msalt = await bcrypt.genSalt(10);
-        const passHashed = await bcrypt.hash(req.body.password, msalt);
-        const userExist = await User.findOne({
-          $or: [{ email: email }, { phone: phone }],
-        });
+        const msalt= await bcrypt.genSalt(10)
+        const passHashed= await bcrypt.hash(password, msalt)
+        const userExist = await User.findOne({email });
         const regUser = await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: passHashed,
+          name:name,
+          email: email,
+          password:passHashed,
         });
         if (userExist) {
           return res
             .status(400)
-            .json({ error: "Email or Phone number already exists" });
+            .json({ error: "Email already exists" });
         }
-
-        const user = await regUser.save();
-        res.status(200).json({
+        const user= await regUser.save();
+        return res.status(200).json({
           success: true,
-          data: user,
+          data: user
         });
       } catch (err) {
-        res.status(500).json({ success: false });
+        res.status(500).json({ success: false ,message:'Server Error'});
       }
   }
 };
